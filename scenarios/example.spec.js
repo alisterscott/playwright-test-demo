e783e90a17f777229084d4dbf1b122757898427c
Alister Scott <ascott@console.com.au>
2021-07-12 20:25:38 +1000

    Add visual comparisons

--- scenarios/example.spec.js
+++ scenarios/example.spec.js
@@ -4,10 +4,12 @@ const { test, expect } = require('@playwright/test')
 test('can wait for an element to appear', async ({ page }) => {
   await nav.visitHomePage(page)
   await page.waitForSelector('#elementappearschild', { visible: true, timeout: 5000 })
+  expect(await page.screenshot()).toMatchSnapshot('element-appears.png')
 })
 
 test('can use an element that appears after on page load', async ({ page }) => {
   await nav.visitHomePage(page)
   const text = await page.textContent('#loadedchild')
   expect(text).toBe('Loaded!')
+  expect(await page.screenshot()).toMatchSnapshot('element-after-onload.png')
 })
