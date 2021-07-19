1faf95978536785a9fac4d43b78672bd32735c1c
Alister Scott <ascott@console.com.au>
2021-07-19 15:49:49 +1000

    Fix config to do retry

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ module.exports = {
   use: {
     headless: true,
     screenshot: 'only-on-failure',
-    video: 'retry-with-video',
-    retries: 2
-  }
+    video: 'retry-with-video'
+  },
+  retries: 1
 }
